dd959cf2a000c5cf69d1065b8781dc9fb8789b2a
abbedqassam924@gmail.com <abbedqassam924@gmail.com>
2023-03-15 23:35:02 +0700

    portfolioku

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,12 @@ const hamburger = documen.querySelector('#hamburger');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active')
 })
+
+function myFunction() {
+  var x = document.getElementById("navbar-default");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
